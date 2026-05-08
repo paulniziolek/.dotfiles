5f041f89b7f89f617f28e8793ea85f15364b8f72
paulniziolek <niziolek.paul@gmail.com>
2026-05-07 22:57:09 -0400

    init wezterm configs

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,13 @@
+local wezterm = require 'wezterm'
+
+local config = wezterm.config_builder()
+
+
+config.initial_cols = 120
+config.initial_rows = 28
+
+config.font_size = 10
+config.color_scheme = 'AdventureTime'
+
+return config
+
